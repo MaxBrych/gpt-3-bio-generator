2b13be046a22d909e008e9f2676bf0263bd069af
MaxBrych <max.brych03@gmail.com>
2023-02-19 22:17:25 +0100

    logo stored on google drive

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
       >
         <Image
           alt="header text"
-          src="/nk_logo.svg"
+          src="https://drive.google.com/uc?export=view&id=1oQSE6663WT_rgxojC6dkjT53tvXT89tu"
           className="w-8 h-8 sm:w-48 sm:h-48"
           width={64}
           height={64}
