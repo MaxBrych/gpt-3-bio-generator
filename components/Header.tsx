c0ce270d3f1c27bf8657cfbb07ee23cfb3188a26
MaxBrych <max.brych03@gmail.com>
2023-02-20 07:35:13 +0100

    logo changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
+import logo from "../public/nk_logo.svg";
 
 export default function Header() {
   return (
-    <header className="fixed top-0  items-center justify-center w-full h-16 px-4 bg-white border-b md:static flex lg:max-w-5xl lg:rounded-t-xl border-dark-95">
+    <header className="fixed top-0 flex items-center justify-center w-full h-16 px-4 bg-white border-b md:static lg:max-w-5xl lg:rounded-t-xl border-dark-95">
       <Link
         href="/"
-        className="flex flex-1 items-center justify-center w-1/2 h-16 py-1 space-x-3"
+        className="flex items-center justify-center flex-1 w-1/2 h-16 py-1 space-x-3"
       >
-        <Image
-          alt="header text"
-          src="https://drive.google.com/uc?export=view&id=1oQSE6663WT_rgxojC6dkjT53tvXT89tu"
-          className="w-8 h-8 sm:w-48 sm:h-48"
-          width={64}
-          height={64}
-        />
+        <div className="relative w-56 h-16">
+          <Image
+            className="object-fit-cover"
+            alt="Nordkurier KI Tools"
+            src={logo}
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
       </Link>
     </header>
   );
