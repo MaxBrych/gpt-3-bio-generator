75d871b858c24791c05b5e90c53f64ac72b5d31c
MaxBrych <max.brych03@gmail.com>
2023-03-26 21:08:24 +0200

    return to older code

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -33,21 +33,22 @@ const Sidebar: React.FC<ToolListProps> = ({ tools }) => {
         } fixed z-10 md:relative md:top-0 top-16 right-0 h-full w-full gap-2 px-4 py-4 bg-white w- md:flex md:flex-col md:gap-2`}
       >
         {tools.map((tool) => (
-          <Link href="/tools/[slug]" as={`/tools/${tool.slug}`} key={tool.id}>
-            <a
-              className={`flex items-center justify-start gap-2 p-2 rounded-full cursor-pointer h-12 transition md:p-4 md:border-cyan-90 ${
-                router.pathname === `/tools/${tool.slug}`
-                  ? "bg-cyan-95"
-                  : "bg-white"
-              }`}
-            >
-              <div className="flex items-center justify-center w-8 h-8 rounded-xl bg-cyan-95">
-                <Image src={tool.icon} alt={tool.name} width={20} height={20} />
-              </div>
-              <h1 className="font-medium leading-3 text-m md:text-sm md:text-left">
-                {tool.name}
-              </h1>
-            </a>
+          <Link
+            href="/tools/[slug]"
+            as={`/tools/${tool.slug}`}
+            key={tool.id}
+            className={`flex items-center justify-start gap-2 p-2 rounded-full cursor-pointer h-12 transition md:p-4 md:border-cyan-90 md:hover:bg-cyan-95 ${
+              router.pathname == `/tools/${tool.slug}`
+                ? "bg-cyan-90"
+                : "bg-white"
+            }`}
+          >
+            <div className="flex items-center justify-center w-8 h-8 rounded-xl bg-cyan-95">
+              <Image src={tool.icon} alt={tool.name} width={20} height={20} />
+            </div>
+            <h1 className="font-medium leading-3 text-m md:text-sm md:text-left">
+              {tool.name}
+            </h1>
           </Link>
         ))}
       </div>
